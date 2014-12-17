00232d155e52874b327e7cb57c78a0b0aee1a065
Stanislav Usenkov <SimSonic@users.noreply.github.com>
2014-12-17 17:10:53 +0600

    Uploaded previously made code.

---
+++ src/main/java/ru/simsonic/rscFirstJoinDemo/BukkitPluginMain.java
@@ -0,0 +1,450 @@
+package ru.simsonic.rscFirstJoinDemo;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.block.Sign;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import ru.simsonic.rscFirstJoinDemo.Trajectory.TrajectoryPlayState;
+import ru.simsonic.rscFirstJoinDemo.Trajectory.TrajectoryPoint;
+import ru.simsonic.utilities.CommandAnswerException;
+import ru.simsonic.utilities.LanguageUtility;
+
+public final class BukkitPluginMain extends JavaPlugin implements Listener
+{
+	protected static final Logger consoleLog = Logger.getLogger("Minecraft");
+	private static final String chatPrefix = "{DARKGREEN}[rscfjd] {LIGHTGREEN}";
+	private static final String defaultTrajectory = "trajectory";
+	private final TrajectoryPlayer trajectoryPlayer = new TrajectoryPlayer(this);
+	protected final HashMap<String, Trajectory> trajectories = new HashMap<>();
+	protected final HashMap<Player, TrajectoryPlayState> playing = new HashMap<>();
+	protected final HashMap<Player, Trajectory> buffers = new HashMap<>();
+	protected String firstJoinTrajectory;
+	@Override
+	public void onLoad()
+	{
+		saveDefaultConfig();
+		switch(getConfig().getInt("internal.version", 1))
+		{
+			case 1:
+				// NEWEST VERSION
+				break;
+			default:
+				// UNSUPPORTED VERSION?
+				break;
+		}
+		consoleLog.log(Level.INFO, "[rscfjd] rscFirstJoinDemo has been loaded.");
+	}
+	@Override
+	public void onEnable()
+	{
+		// Read settings 
+		reloadConfig();
+		firstJoinTrajectory = getConfig().getString("settings.trajectory", defaultTrajectory);
+		getConfig().set("settings.trajectory", firstJoinTrajectory);
+		saveConfig();
+		// Register event's dispatcher
+		getServer().getPluginManager().registerEvents(this, this);
+		/*
+		// ProtocolLib
+		try
+		{
+			final org.bukkit.plugin.BukkitPluginMain plib = getServer().getPluginManager().getPlugin("ProtocolLib");
+			if(plib != null && plib instanceof ProtocolLibrary)
+			{
+				ProtocolLibrary.getProtocolManager().addPacketListener(
+					new PacketAdapter(this, ConnectionSide.SERVER_SIDE, Packets.Server.CHAT)
+					{
+						@Override
+						public void onPacketSending(PacketEvent event)
+						{
+							// КРОМЕ ТОГО СЛУЧАЯ, КОГДА МЫ ПИШЕМ СООБЩЕНИЕ ИГРОКУ —— TO DO HERE !!!
+							// if(playing.containsKey(event.getPlayer()))
+								// event.setCancelled(true);
+						}
+					});
+				consoleLog.log(Level.INFO, "[rscfjd] ProtocolLib was found and integrated.");
+			}
+		} catch(RuntimeException ex) {
+		}
+		*/
+		// Done
+		consoleLog.log(Level.INFO, "[rscfjd] rscFirstJoinDemo has been successfully enabled.");
+	}
+	@Override
+	public void onDisable()
+	{
+		getServer().getServicesManager().unregisterAll(this);
+		getServer().getScheduler().cancelTasks(this);
+		for(Player demo : playing.keySet())
+			trajectoryPlayer.finishDemo(demo);
+		saveConfig();
+		buffers.clear();
+		playing.clear();
+		trajectories.clear();
+		consoleLog.info("[rscfjd] rscFirstJoinDemo has been disabled.");
+	}
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event)
+	{
+		final Player player = event.getPlayer();
+		// Hide all other demo players
+		for(Player demo : playing.keySet())
+			player.hidePlayer(demo);
+		// Check if we should show the demo for him
+		if(!player.hasPlayedBefore())
+		{
+			if(player.hasPermission("rscfjd.admin"))
+			{
+				player.sendMessage(LanguageUtility.processStringStatic(chatPrefix + "You have skipped demo due to having admin permission."));
+				consoleLog.log(Level.INFO, "[rscfjd] Skipping player {0} due to admin permission.", player.getDisplayName());
+				return;
+			}
+			if(lazyFirstJoinTrajectoryLoading())
+				trajectoryPlayer.beginDemo(player, trajectories.get(firstJoinTrajectory));
+		}
+	}
+	@EventHandler
+	public void onPlayerChat(AsyncPlayerChatEvent event)
+	{
+		if(playing.containsKey(event.getPlayer()))
+			event.setCancelled(true);
+		else
+			event.getRecipients().removeAll(playing.keySet());
+	}
+	@EventHandler
+	public void onPlayerCommand(PlayerCommandPreprocessEvent event)
+	{
+		final Player player = event.getPlayer();
+		if(playing.containsKey(player) && !player.hasPermission("rscfjd.admin"))
+			event.setCancelled(true);
+	}
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event)
+	{
+		trajectoryPlayer.finishDemo(event.getPlayer());
+	}
+	@EventHandler
+	public void onPlayerKick(PlayerKickEvent event)
+	{
+		trajectoryPlayer.finishDemo(event.getPlayer());
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		try
+		{
+			switch(label.toLowerCase())
+			{
+				case "rscfjd":
+					execute(sender, args);
+					break;
+			}
+		} catch(CommandAnswerException ex) {
+			for(String answer : ex.getMessageArray())
+				sender.sendMessage(LanguageUtility.processStringStatic(chatPrefix + answer));
+		}
+		return true;
+	}
+	private boolean lazyFirstJoinTrajectoryLoading()
+	{
+		// Lazy trajectory loading
+		if(trajectories.containsKey(firstJoinTrajectory) == false)
+			loadTrajectory(firstJoinTrajectory);
+		return trajectories.containsKey(firstJoinTrajectory);
+	}
+	private Trajectory loadTrajectory(String caption)
+	{
+		Trajectory result;
+		caption = caption.toLowerCase();
+		// Load default currentTrajectory
+		try(FileInputStream fis = new FileInputStream(new File(getDataFolder(), caption + ".json")))
+		{
+			final JsonReader jr = new JsonReader(new InputStreamReader(fis, "UTF-8"));
+			result = new Gson().fromJson(jr, Trajectory.class);
+		} catch(IOException | JsonParseException ex) {
+			result = null;
+			consoleLog.log(Level.WARNING, "[rscfjd] Error reading {0}.json: {1}", new Object[] { caption, ex.toString() });
+		}
+		if(result == null)
+			result = new Trajectory();
+		if(result.points == null)
+			result.points = new TrajectoryPoint[] {};
+		for(TrajectoryPoint tp : result.points)
+			tp.location = trajectoryPlayer.locationForTrajectoryPoint(tp);
+		consoleLog.log(Level.INFO, "[rscfjd] Trajectory {0} has been loaded ({1})", new Object[] { caption, result.points.length });
+		result.caption = caption;
+		trajectories.put(caption, result);
+		return result;
+	}
+	private void saveTrajectory(Trajectory trajectory, String caption)
+	{
+		if(trajectory == null)
+			return;
+		caption = caption.toLowerCase();
+		try(JsonWriter jw = new JsonWriter(
+			new OutputStreamWriter(new FileOutputStream(
+				new File(getDataFolder(), caption + ".json")), "UTF-8")))
+		{
+			jw.setIndent(" ");
+			jw.setSerializeNulls(false);
+			new Gson().toJson(trajectory, Trajectory.class, jw);
+			consoleLog.log(Level.INFO, "[rscfjd] Trajectory {0} has been saved ({1})", new Object[] { caption, trajectory.points.length });
+		} catch(IOException | JsonParseException ex) {
+			consoleLog.log(Level.WARNING, "[rscfjd] Error writing {0}.json: {1}", new Object[] { caption, ex.toString() });
+		}
+	}
+	private final String signFirstLine = LanguageUtility.processStringStatic("{_DG}[rscFJD]");
+	@org.bukkit.event.EventHandler
+	public void onSignChange(final SignChangeEvent event)
+	{
+		if(event.getLine(0).equalsIgnoreCase("[rscfjd]") == false)
+			return;
+		final Player player = event.getPlayer();
+		if(!player.hasPermission("rscfjd.admin"))
+		{
+			player.sendMessage(LanguageUtility.processStringStatic(chatPrefix + "{_LR}Not enough permissions."));
+			event.setCancelled(true);
+			return;
+		}
+		event.setLine(0, signFirstLine);
+		final String flight = event.getLine(1).isEmpty() ? firstJoinTrajectory : event.getLine(1);
+		event.setLine(1, LanguageUtility.processStringStatic(
+			getConfig().getString("settings.signs.note", "{_LG}Start demo")));
+		event.setLine(3, flight);
+		player.sendMessage(LanguageUtility.processStringStatic(chatPrefix + "{_LG}Done."));
+	}
+	@org.bukkit.event.EventHandler
+	public void onSignRBClick(final PlayerInteractEvent event)
+	{
+		if(event.getAction() != Action.RIGHT_CLICK_BLOCK)
+			return;
+		if(!(event.getClickedBlock().getState() instanceof Sign))
+			return;
+		final Sign sign = (Sign)event.getClickedBlock().getState();
+		if(!sign.getLine(0).equals(signFirstLine))
+			return;
+		final String trajectoryName = sign.getLine(3);
+		final Trajectory trajectory = loadTrajectory(trajectoryName);
+		trajectoryPlayer.beginDemo(event.getPlayer(), trajectory);
+	}
+	private Trajectory getBufferedTrajectory(Player player)
+	{
+		if(buffers.containsKey(player))
+			return buffers.get(player);
+		final Trajectory result = new Trajectory();
+		buffers.put(player, result);
+		return result;
+	}
+	private void setBufferedTrajectory(Player player, Trajectory buffer)
+	{
+		buffers.put(player, buffer);
+	}
+	private void execute(CommandSender sender, String[] args) throws CommandAnswerException
+	{
+		if(args.length == 0)
+			throw new CommandAnswerException("{MAGENTA}rscFirstJoinDemo {_LS}" + getDescription().getVersion() + "{MAGENTA} by SimSonic.");
+		final String command = args[0].toLowerCase();
+		args = Arrays.copyOfRange(args, 1, (args.length >= 4) ? args.length : 4);
+		switch(command)
+		{
+		case "load":
+			if(sender.hasPermission("rscfjd.admin"))
+			{
+				if(sender instanceof Player)
+				{
+					final String filename = (args[0] != null && !"".equals(args[0])) ? args[0] : defaultTrajectory;
+					final Trajectory buffer = loadTrajectory(filename);
+					setBufferedTrajectory((Player)sender, buffer);
+					throw new CommandAnswerException("Loaded (" + buffer.points.length + " nodes)");
+				}
+				throw new CommandAnswerException("This command cannot be run from console.");
+			}
+			break;
+		case "save":
+			if(sender.hasPermission("rscfjd.admin"))
+			{
+				if(sender instanceof Player)
+				{
+					final String filename = (args[0] != null && !"".equals(args[0])) ? args[0] : defaultTrajectory;
+					saveTrajectory(getBufferedTrajectory((Player)sender), filename);
+					throw new CommandAnswerException("Saved {_LC}" + filename + ".json{_LG}.");
+				}
+				throw new CommandAnswerException("This command cannot be run from console.");
+			}
+			break;
+		case "clear":
+			if(sender.hasPermission("rscfjd.admin"))
+			{
+				if(sender instanceof Player)
+				{
+					final Trajectory buffer = new Trajectory();
+					buffer.points = new TrajectoryPoint[] {};
+					setBufferedTrajectory((Player)sender, buffer);
+					throw new CommandAnswerException("{_LG}Buffer cleared.");
+				}
+				throw new CommandAnswerException("This command cannot be run from console.");
+			}
+			break;
+		case "play":
+			if(sender.hasPermission("rscfjd.admin"))
+			{
+				Player play_player;
+				if(args[0] != null && !"".equals(args[0]))
+					play_player = getServer().getPlayer(args[0]);
+				else if(sender instanceof Player)
+					play_player = (Player)sender;
+				else
+					throw new CommandAnswerException("Player-only command.");
+				if(play_player == null)
+					throw new CommandAnswerException("Cannot find such player.");
+				if(buffers.containsKey(play_player))
+					trajectoryPlayer.beginDemo(play_player, buffers.get(play_player));
+				else
+					if(lazyFirstJoinTrajectoryLoading())
+						trajectoryPlayer.beginDemo(play_player, trajectories.get(firstJoinTrajectory));
+				return;
+			}
+			break;
+		case "stop":
+			if(sender.hasPermission("rscfjd.admin"))
+			{
+				Player stop_player;
+				if(args[0] != null && !"".equals(args[0]))
+					stop_player = getServer().getPlayer(args[0]);
+				else if(sender instanceof Player)
+					stop_player = (Player)sender;
+				else
+					throw new CommandAnswerException("Player-only command.");
+				if(stop_player == null)
+					throw new CommandAnswerException("Cannot find such player.");
+				trajectoryPlayer.finishDemo(stop_player);
+				return;
+			}
+			break;
+		// addpoint <freeze ticks> <set speed> [message]
+		case "addpoint":
+			if(!(sender instanceof Player))
+				throw new CommandAnswerException("Player-only command.");
+			if(sender.hasPermission("rscfjd.admin"))
+			{
+				final Trajectory buffer = getBufferedTrajectory((Player)sender);
+				final Player me = (Player)sender;
+				final ArrayList<TrajectoryPoint> pointList = new ArrayList<>();
+				if(buffer.points != null)
+					pointList.addAll(Arrays.asList(buffer.points));
+				final TrajectoryPoint tp = new TrajectoryPoint(me.getLocation());
+				try
+				{
+					tp.freezeTicks = Integer.parseInt(args[0]);
+				} catch(NumberFormatException ex) {
+					tp.freezeTicks = 0;
+				}
+				try
+				{
+					tp.speedAfter = Float.parseFloat(args[1]);
+				} catch(NumberFormatException ex) {
+					tp.speedAfter = 1.0f;
+				}
+				tp.messageOnReach = LanguageUtility.glue(Arrays.copyOfRange(args, 2, args.length), " ");
+				tp.fly = true;
+				pointList.add(tp);
+				buffer.points = pointList.toArray(new TrajectoryPoint[pointList.size()]);
+				throw new CommandAnswerException("Done #" + pointList.size());
+			}
+			break;
+		case "tp":
+			if(!(sender instanceof Player))
+				throw new CommandAnswerException("Player-only command.");
+			if(sender.hasPermission("rscfjd.admin"))
+			{
+				final Player me = (Player)sender;
+				final Trajectory buffer = getBufferedTrajectory(me);
+				int teleport_id;
+				try
+				{
+					teleport_id = Integer.parseInt(args[0]);
+				} catch(NumberFormatException ex) {
+					teleport_id = buffer.points.length - 1;
+				}
+				if(teleport_id >= 0 && teleport_id < buffer.points.length)
+				{
+					me.teleport(buffer.points[teleport_id].location);
+					throw new CommandAnswerException("Teleported to #" + teleport_id);
+				}
+				throw new CommandAnswerException("{_LR}Out of range (0..." + (buffer.points.length - 1) + ").");
+			}
+			break;
+		case "info":
+			if(sender.hasPermission("rscfjd.admin"))
+			{
+				final ArrayList<String> answer = new ArrayList<>();
+				answer.add("Current configuration:");
+				answer.add("firstJoinTrajectory: {_YL}" + firstJoinTrajectory);
+				answer.add("firstJoinTrajectory is " + (trajectories.containsKey(firstJoinTrajectory)
+					? "{_DR}not loaded yet"
+					: "{_DG}already loaded"));
+				if(trajectories.containsKey(firstJoinTrajectory))
+					answer.add("firstJoinTrajectory contains points: {WHITE}"
+						+ trajectories.get(firstJoinTrajectory).points.length);
+				if(buffers.containsKey((Player)sender))
+					answer.add("Your have some trajectory points in your buffer: {WHITE}"
+					+ buffers.get((Player)sender).points.length);
+				else
+					answer.add("Your have no trajectory points in your buffer");
+				throw new CommandAnswerException(answer);
+			}
+			break;
+		case "help":
+			if(sender.hasPermission("rscfjd.admin"))
+				throw new CommandAnswerException(new String[]
+				{
+					"Usage:",
+					"{YELLOW}/rscfjd play [player name]",
+					"{YELLOW}/rscfjd stop [player name]",
+					"{YELLOW}/rscfjd addpoint <freeze ticks> <speed after (bps)> [text w/format]",
+					"{YELLOW}/rscfjd save [caption] {_LS}- save your buffer into file",
+					"{YELLOW}/rscfjd load [caption] {_LS}- load file into your buffer",
+					"{YELLOW}/rscfjd tp <#> {_LS}- teleport on your buffer's point #",
+					"{YELLOW}/rscfjd clear {_LS}- clears your buffer",
+					"{YELLOW}/rscfjd reload",
+				});
+			break;
+		case "reload":
+			if(sender.hasPermission("rscfjd.admin"))
+			{
+				reloadConfig();
+				getPluginLoader().disablePlugin(this);
+				getPluginLoader().enablePlugin(this);
+				getServer().getConsoleSender().sendMessage("[rscfjd] rscFirstJoinDemo has been reloaded.");
+				return;
+			}
+			break;
+		}
+		throw new CommandAnswerException("{_LR}Not enough permissions.");
+	}
+}
---
+++ src/main/java/ru/simsonic/rscFirstJoinDemo/TrajectoryPlayer.java
@@ -0,0 +1,213 @@
+package ru.simsonic.rscFirstJoinDemo;
+import java.util.logging.Level;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.WeatherType;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
+import ru.simsonic.rscFirstJoinDemo.Trajectory.TrajectoryPlayState;
+import ru.simsonic.rscFirstJoinDemo.Trajectory.TrajectoryPoint;
+import ru.simsonic.utilities.LanguageUtility;
+
+public class TrajectoryPlayer
+{
+	private final BukkitPluginMain plugin;
+	TrajectoryPlayer(BukkitPluginMain plugin)
+	{
+		this.plugin = plugin;
+	}
+	public void beginDemo(final Player player, final Trajectory trajectory)
+	{
+		// Cancel old demo if it was
+		finishDemo(player);
+		// Hide player from all
+		for(Player online : plugin.getServer().getOnlinePlayers())
+			online.hidePlayer(player);
+		try
+		{
+			// Start flight
+			final TrajectoryPlayState tps = trajectory.newPlayState();
+			if(tps.trajectory.points.length <= 0)
+			{
+				BukkitPluginMain.consoleLog.log(Level.INFO, "[rscfjd] Cannot run demo for {0}, it is empty.", player.getDisplayName());
+				return;
+			}
+			tps.currentPoint = -1;
+			tps.originalFlightAllow = player.getAllowFlight();
+			tps.originalFlightState = player.isFlying();
+			player.setAllowFlight(true);
+			player.setFlying(true);
+			player.setPlayerWeather(WeatherType.CLEAR);
+			plugin.playing.put(player, tps);
+			tps.playTask = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					tps.localTick += 1;
+					processDemoStep(player, tps);
+				}
+			}, 1, 1);
+			BukkitPluginMain.consoleLog.log(Level.INFO, "[rscfjd] Starting playing demo '{0}' to {1}",
+				new Object[] { tps.trajectory.caption, player.getDisplayName() });
+		} catch(RuntimeException ex) {
+			BukkitPluginMain.consoleLog.log(Level.SEVERE, "[rscfjd] Demo starting error: {0}", new Object[] { ex });
+		}
+	}
+	public void finishDemo(Player player)
+	{
+		try
+		{
+			final TrajectoryPlayState tps = plugin.playing.get(player);
+			if(tps != null)
+			{
+				player.setAllowFlight(tps.originalFlightAllow);
+				player.setFlying(tps.originalFlightState);
+				player.resetPlayerWeather();
+				player.resetPlayerTime();
+				plugin.getServer().getScheduler().cancelTask(tps.playTask);
+				final int points = tps.trajectory.points.length;
+				if(points > 0)
+				{
+					player.teleport(tps.trajectory.points[points - 1].location);
+					player.saveData();
+				}
+				plugin.playing.remove(player);
+				BukkitPluginMain.consoleLog.log(Level.INFO, "[rscfjd] Finished playing demo to {0}", player.getDisplayName());
+			}
+			for(Player online : plugin.getServer().getOnlinePlayers())
+				online.showPlayer(player);
+		} catch(RuntimeException ex) {
+			BukkitPluginMain.consoleLog.log(Level.SEVERE, "[rscfjd] Demo stopping error: {0}", new Object[] { ex });
+		}
+	}
+	private void processDemoStep(final Player player, final TrajectoryPlayState tps)
+	{
+		if(!player.isOnline())
+		{
+			finishDemo(player);
+			return;
+		}
+		try
+		{
+			// Are we in the beginning?
+			if(tps.started == false)
+			{
+				tps.currentPointStartTick = tps.localTick;
+				tps.started = true;
+			}
+			// Is current segment finished?
+			if(tps.localTick - tps.currentPointStartTick >= tps.currentSegmentFlightTime)
+			{
+				// I should look for next point with location != null
+				for(tps.currentPoint += 1; tps.currentPoint < tps.trajectory.points.length; tps.currentPoint += 1)
+					if(tps.trajectory.points[tps.currentPoint].location != null)
+						break;
+				// Was it the last segment?
+				if(tps.currentPoint >= tps.trajectory.points.length)
+				{
+					finishDemo(player);
+					return;
+				}
+				tps.currentPointStartTick = tps.localTick;
+				// Isn't it the last point?
+				final int nextPossiblePoint = tps.currentPoint + 1;
+				final TrajectoryPoint tp1 = tps.trajectory.points[tps.currentPoint];
+				final TrajectoryPoint tp2 = (nextPossiblePoint < tps.trajectory.points.length)
+					? tps.trajectory.points[nextPossiblePoint]
+					: null;
+				tps.currentSegmentFlightTime = calculateFlightTime(tp1, tp2);
+				tps.deltaYaw = calculateYawDelta(tp1, tp2);
+				// Message on point reach
+				if(tp1.messageOnReach != null && !"".equals(tp1.messageOnReach))
+					player.sendMessage(LanguageUtility.processStringStatic(tp1.messageOnReach));
+				BukkitPluginMain.consoleLog.log(Level.INFO, "[rscfjd] Player {0} has reached demo point #{1}", new Object[]
+				{
+					player.getDisplayName(),
+					tps.currentPoint
+				});
+				// Time and weather tricks
+				if(tp1.timeReset)
+					player.resetPlayerTime();
+				else
+					if(tp1.timeUpdate)
+						player.setPlayerTime(tp1.timeUpdateValue, !tp1.timeUpdateLock);
+				if(tp1.weatherReset)
+					player.resetPlayerWeather();
+				else
+					if(tp1.weatherUpdate)
+						player.setPlayerWeather(tp1.weatherUpdateStormy ? WeatherType.DOWNFALL : WeatherType.CLEAR);
+			}
+			final TrajectoryPoint tp1 = tps.trajectory.points[tps.currentPoint];
+			player.setAllowFlight(true);
+			player.setFlying(tp1.fly);
+			long currentSegmentTimeSpent = tps.localTick - tps.currentPointStartTick;
+			// Teleport player to the next position
+			if(currentSegmentTimeSpent >= tp1.freezeTicks)
+			{
+				currentSegmentTimeSpent -= tp1.freezeTicks;
+				final TrajectoryPoint tp2 = tps.trajectory.points[tps.currentPoint + 1];
+				final double percent = (tps.currentSegmentFlightTime != 0)
+					? currentSegmentTimeSpent * 1.0 / tps.currentSegmentFlightTime
+					: 1.0;
+				final Location target = tp2.location.clone();
+				final World w1 = tp1.location.getWorld();
+				final World w2 = tp2.location.getWorld();
+				if(w1 != null && w2 != null && w1.equals(w2))
+				{
+					// Find position
+					target.subtract(tp1.location);
+					target.multiply(percent);
+					target.add(tp1.location);
+					// Find rotation
+					float fp1 = tp1.location.getPitch(), fy1 = tp1.location.getYaw();
+					target.setPitch((float)(fp1 + percent * (tp2.location.getPitch() - fp1)));
+					target.setYaw((float)(fy1 + percent * tps.deltaYaw));
+				}
+				// Teleport
+				player.teleport(target, TeleportCause.PLUGIN);
+			} else
+				player.teleport(tp1.location, TeleportCause.PLUGIN);
+		} catch(RuntimeException ex) {
+			BukkitPluginMain.consoleLog.log(Level.SEVERE, "[rscfjd] Demo processing error: {0}", new Object[] { ex });
+			finishDemo(player);
+		}
+	}
+	private long calculateFlightTime(TrajectoryPoint tp1, TrajectoryPoint tp2)
+	{
+		long result = tp1.freezeTicks;
+		if(tp1.speedAfter > 0.005f && tp2 != null)
+		{
+			final Location l1 = tp1.location;
+			final Location l2 = tp2.location;
+			if(l1 != null && l2 != null)
+			{
+				final World w1 = l1.getWorld();
+				final World w2 = l2.getWorld();
+				if(w1 != null && w2 != null && w1.equals(w2))
+					result += Math.ceil(0.5 + l1.distance(l2) * 20.0f / tp1.speedAfter);
+			}
+		}
+		return result;
+	}
+	private float calculateYawDelta(TrajectoryPoint tp1, TrajectoryPoint tp2)
+	{
+		if(tp2 == null)
+			return 0.0f;
+		float result = tp2.location.getYaw() - tp1.location.getYaw();
+		if(result >= 180.0f)
+			result -= 360.0f;
+		if(result <= -180.0f)
+			result += 360.0f;
+		return result;
+	}
+	public Location locationForTrajectoryPoint(TrajectoryPoint tp)
+	{
+		final World world = plugin.getServer().getWorld(tp.world);
+		if(world != null)
+			return new Location(world, tp.x, tp.y, tp.z, tp.yaw, tp.pitch);
+		BukkitPluginMain.consoleLog.log(Level.SEVERE, "[rscfjd] World not found: {0}", tp.world);
+		return null;
+	}
+}
